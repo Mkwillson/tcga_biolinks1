929af3f28af77454af5fea9a3510946840a66f08
Your Name <megankw02@gmail.com>
2023-02-16 11:14:18 +0000

    .

--- Mutation.manipulation.R
+++ Mutation.manipulation.R
@@ -8,32 +8,39 @@ BiocManager::install("biomaRt")
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
 
 # Build a biomart query
-
-listEnsemblGenomes()
-
-filters = listFilters(ensembl)
-filters[1:5,]
-
-attributes = listAttributes(ensembl)
-attributes[1:5,]
-
-searchFilters(mart = ensembl, pattern = "ensembl.*id")
-
-mart <- useEnsembl(dataset = "hsapiens_gene_ensembl", biomart='ensembl')
-head(keytypes(mart), n = 3)
-
 brca.data$mutational$Entrez_Gene_Id
 
-ensembl = getBM(attributes = c('entrezgene_id', 'ensembl_gene_id'), 
-              filters = 'entrezgene_id', 
-              values = brca.data$mutational$Entrez_Gene_Id, 
-              mart = ensembl)
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+genes <- getBM(
+  filters="entrezgene_id",
+  attributes=c("ensembl_gene_id", "entrezgene_id"),
+  values=brca.data$mutational$Entrez_Gene_Id,
+  mart=mart)
+genes
 
 ###################################
+
 library(dplyr)
 
 # Select for specific column headings
 head(brca.data$mutational)
 BRCAmutations <- dplyr::select(brca.data$mutational, Hugo_Symbol, Entrez_Gene_Id, Gene, Variant_Classification, SIFT, PolyPhen)
 BRCAmutations
-brca.data$mutational$Consequence
+table(BRCAmutations$Gene)
+
+# Calculate Frequency of Gene
+
+
+help("aggregate")
+aggregate(brca.data$mutational$Hugo_Symbol, by=list(Category=x$Category), FUN=sum)
+###################################
+
+# Compare 2 lists of ensemble genes
+genes$ensembl_gene_id
+BRCAmutations$Gene
+genes$ensembl_gene_id %in% BRCAmutations$Gene
+
+NumberTRUE <- table(genes$ensembl_gene_id %in% BRCAmutations$Gene)["TRUE"]
+NumberFALSE <- table(genes$ensembl_gene_id %in% BRCAmutations$Gene)["FALSE"]
+(NumberFALSE/NumberTRUE)*100
